6781afc36419d152329eae3c8d67818ed19610f4
Esduardo Del Aguila <daguilae@yahoo.com>
2022-02-11 09:49:46 -0600

    Se agrego el tema funciones

---
+++ Funciones/Funciones.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintCuteMessage()
+{
+    cout << "Eres increible!" << endl;
+}
+
+void Print(string message)
+{
+    cout << message << endl;
+}
+
+bool IsPlayerDead(int hp)
+{
+    if(hp <= 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool PrintBool(bool value)
+{
+    if(value == true)
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+}
+
+int GetAttackPower(int Damage, int Weapon)
+{
+    return Damage + Weapon;
+}
+
+int main()
+{
+    PrintCuteMessage();
+    PrintCuteMessage();
+
+    int hp = 20;
+    int power = 5;
+    int weapon  = 11;
+
+    Print("Is Player dead????");
+
+    bool isDead = IsPlayerDead( hp - GetAttackPower(power,weapon) );
+
+    PrintBool(isDead);
+    Print("Este es el valor real de acuerdo a la compu");
+    cout << isDead << endl;
+
+    return 0;
+}
